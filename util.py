0d140e0bad27231547c4ed1181aa76d55b0f0d08
XudongX <xudong714@gmail.com>
2019-12-17 12:50:05 +0800

    add docstring in ThreadDecorator

--- util.py
+++ util.py
@@ -9,18 +9,34 @@ logger = logging.getLogger('util.py')  # 不加名称设置root logger
 
 
 class ThreadDecorator(Thread):
-    def __init__(self, thread_func, *args, threads_q=None, **kw):
+    """
+    ThreadDecorator extends threading.Thread
+    run a function in a new thread, automatically catch Runtime Error in the thread
+    logging the error and put thread back to threads queue if it exist.
+    """
+
+    def __init__(self, target_func, *args, threads_q=None, **kw):
+        """
+        :param target_func:
+        :param args:
+        :param threads_q: threads queue
+        :param kw:
+        """
         Thread.__init__(self)
-        self._func = thread_func
+        self._func = target_func
         self._q = threads_q
         self._args = args
         self._kw = kw
 
     def run(self) -> None:
+        """
+        override threading.Thread.run()
+        :return:
+        """
         try:
             self._func(*self._args, **self._kw)
         except RuntimeError as e:
-            logging.critical(">>>> Unknown Runtime Error raised")
+            logging.critical(">>>> Unknown Runtime Error arose")
             logging.critical(traceback.format_exc())
             if self._q is not None:
                 logging.error(">>>> Put thread func back to threads queue:")
